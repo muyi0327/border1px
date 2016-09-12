005daf8ae99da080fd232755770904c42c15a95f
muyi0327 <yfdezhuye@163.com>
2016-09-12 11:30:50 +0800

    update example and readme

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,16 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var less = require('gulp-less');
 
-var origin = gulp.src('./example/**/*.scss');
-var dest = gulp.dest('./example/');
+var dest = gulp.dest('./example/css');
 
-gulp.task('sass', function(){
-    return origin.pipe(sass({outputStyle:'expanded', sourceComments: false})).pipe(dest);
+gulp.task('sass', function () {
+    return gulp.src('./example/sass/**/*.scss')
+        .pipe(sass({ outputStyle: 'expanded', sourceComments: false }))
+        .pipe(dest);
 });
 
-gulp.task('less', function(){
-    return origin.pipe(less()).pipe(dest);
+gulp.task('less', function () {
+    return gulp.src('./example/less/**/*.less')
+        .pipe(less())
+        .pipe(dest);
 });
